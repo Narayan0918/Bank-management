eebccbd0ed245d515ff02ef1c317d96cd016a8bf
Narayan <narayanrajdubey9889@gmail.com>
2026-02-05 15:15:58 +0530

    changed models.py for automatic account number generation and added a form for pin setup for the account

--- myApp/models.py
+++ myApp/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+import random
+
+def generate_account_number():
+    while True:
+        acc_num = random.randint(999210000000,999999999999)
+        if not BankDetails.objects.filter(accountNumber = acc_num).exist():
+            return acc_num
+
 
 # Create your models here.
 class BankDetails(models.Model):
-    accountNumber = models.BigAutoField(primary_key=True) 
+    accountNumber = models.BigAutoField(primary_key=True,default=generate_account_number,editable=False) 
     accountHolder = models.CharField(max_length=40)
     balance = models.FloatField(default=0)
-    pin = models.IntegerField()
+    pin = models.IntegerField(null=True,blank=True)
     bankName = models.CharField(max_length=15)
     mobile = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.accountNumber} - {self.accountHolder}"
